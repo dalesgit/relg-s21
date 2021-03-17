ea2eaa65945f126a860d3c8e1f220fb6b8d1b118
dalesgit <dhath12@gmail.com>
2021-03-17 10:35:09 -0400

    get references

---
+++ 317-s21/present-org/auto/10-1-theology-new-philosophies.el
@@ -0,0 +1,51 @@
+(TeX-add-style-hook
+ "10-1-theology-new-philosophies"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "theology-and-recent-philosophies"
+    "wittgenstein"
+    "deconstruction"
+    "american-response"
+    "process-theology"
+    "hope--eschatology"
+    "liberation-theology"
+    "theology-and-freedom"
+    "sec:org4a56f4c"
+    "sec:orgd5e5ac4"
+    "theology-and-the-secular"
+    "theology--the-secular"
+    "world-of-many-religions"
+    "sec:org6e752b7")
+   (LaTeX-add-bibliographies
+    "../../../../Dropbox/org-roam/winthrop-library"))
+ :latex)
+
